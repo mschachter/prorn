80e3ce68258cd65774062dff2f84676d968f512c
mschachter <mike@mindmech.com>
2012-02-14 10:13:33 -0800

    feedforward connectivity...

--- src/python/prorn/examples.py
+++ src/python/prorn/examples.py
@@ -31,7 +31,7 @@ def create_inputless_net():
     return net
 
 def create_fullyconnected_net(num_nodes, rescale_frac=0.999, noise_std=0.0, random_initial_state=False,
-                              symmetric=False):
+                              symmetric=False, feedforward=False):
     
     net = EchoStateNetwork()
     net.noise_std = noise_std
@@ -49,6 +49,11 @@ def create_fullyconnected_net(num_nodes, rescale_frac=0.999, noise_std=0.0, rand
             for k2 in range(k1):
                 W[k2, k1] = W[k1, k2]
     
+    if feedforward:
+        for k1 in range(num_nodes):
+            for k2 in range(k1+1):
+                W[k1, k2] = 0.0
+    
     for k1 in range(num_nodes):
         for k2 in range(num_nodes):
             net.connect_nodes(k1, k2, W[k1, k2])
@@ -58,101 +63,6 @@ def create_fullyconnected_net(num_nodes, rescale_frac=0.999, noise_std=0.0, rand
     return net
    
 
-def create_3node_net(input_file='/home/cheese63/test.csv'):
-    
-    net = EchoStateNetwork()
-
-    net.create_node(1)
-    net.create_node(2)
-    net.create_node(3)
-    
-    net.connect_nodes(1, 2, -0.5)
-    net.connect_nodes(1, 3, -0.25)
-    net.connect_nodes(2, 3, 0.9)
-    net.connect_nodes(3, 1, -0.037)
-    
-    cis = CsvInputStream([1, 3], input_file)
-    
-    net.set_input(cis)    
-    net.create_input(0)
-    net.create_input(1)
-    net.create_input(2)
-
-    net.connect_input(0, 1, 0.25)
-    net.connect_input(1, 2, 0.12)
-    net.connect_input(2, 3, 0.72)
-    
-
-    net.compile()
-    
-    return net
-
-
-
-def show_stims():
-    
-    stim_len = 10
-    t = np.arange(0, stim_len)
-    
-    bump_times = [5,3]
-    bump_heights = [1,0.5]
-    bump_widths = [0.1,0.05]
-    nsamps = 5
-    
-    stims = gen_1d_stim_family(stim_len, bump_times, bump_heights, bump_widths, num_samps=nsamps)
-    
-    for k in range(nsamps):
-        plt.plot(t, stims[k, :])
-        
-    plt.show()
-
-    
-def run_example_net():
-    
-    net = create_fullyconnected_net()
-    stims = read_stims_from_file('/home/cheese63/git/prorn/data/stims.h5')    
-    net_stims = run_sim(net, stims)
-    
-    return net_stims
-
-            
-def run_many_nets(output_file, num_nets=5, rescale_frac=0.75, index_offset=0):
-    
-    stims = read_stims_from_file('/home/cheese63/git/prorn/data/stims.h5')
-    
-    f = h5py.File(output_file, 'a')
-    
-    nis = NullInputStream([1, 1])
-    
-    burn_time = 100
-    post_stim_time = 20
-    pre_stim_record = 10 
-    start_record = burn_time - pre_stim_record
-    stim_start = burn_time - start_record #relative to recording time start
-    
-    for k in range(num_nets):
-        
-        net_num =k + index_offset
-        print 'Running net %d...' % net_num
-        net_key = 'net_%d' % net_num
-        net = create_fullyconnected_net(rescale_frac=rescale_frac)
-        net_stims = run_sim(net, stims,
-                             burn_time=burn_time, post_stim_time=post_stim_time, start_record=start_record)
-         
-        net.set_input(nis)
-        net.compile()
-        net.to_hdf5(f, net_key)
-         
-        for stim_key,net_state in net_stims.iteritems():
-            
-            stimlen = stims[stim_key].shape[1]
-            stim_end = stim_start + stimlen            
-            f[net_key][stim_key] = net_state
-            f[net_key][stim_key].attrs['stim_start'] = stim_start
-            f[net_key][stim_key].attrs['stim_end'] = stim_end
-    
-    f.close()
-    
 
 def create_morse_networks(net_file, num_nets=1, index_offset=0, num_nodes=3, input_gain=1.0):
     
@@ -230,7 +140,7 @@ def run_morse_nets(stim_file, net_file, input_gain=1.0, noise_std=0.0, num_trial
 def run_morse_nets_online(stim_file, num_trials=20, stack_size=5,
                           num_nodes=3, input_gain=1.0, noise_std=0.0, fixed_seed=None,
                           stim_class='standard', num_nets=10, output_file=None,
-                          symmetric=False):
+                          symmetric=False, feedforward=False):
     
     stimset = MorseStimSet()
     stimset.from_hdf5(stim_file)
@@ -256,7 +166,8 @@ def run_morse_nets_online(stim_file, num_trials=20, stack_size=5,
     for k in range(num_nets):
             
         #read network weights from hdf5
-        net = create_fullyconnected_net(num_nodes=num_nodes, symmetric=symmetric)        
+        net = create_fullyconnected_net(num_nodes=num_nodes, symmetric=symmetric,
+                                        feedforward=feedforward)        
         net.noise_std = noise_std
         
         #try out each input node, see which one works best
@@ -264,6 +175,8 @@ def run_morse_nets_online(stim_file, num_trials=20, stack_size=5,
         best_input_node = 0
 
         for input_node in range(num_nodes):
+            if feedforward and input_node != 0:
+                continue
             
             net_copy = copy.deepcopy(net)
             net_copy.create_input(0)
