023260360c688d76e67529ec92daa726a5022f63
mschachter <mike2>
2012-02-25 09:49:45 -0800

    Started using sympy for expression of symbolic Schur system

--- src/python/prorn/algebra.py
+++ src/python/prorn/algebra.py
@@ -1,37 +1,53 @@
+import re
+
 import numpy as np
 
-class SystemOfEquations():
-    def __init__(self):
-        self.current_matrix = None
+from sympy import Symbol, I, symbols, im
+
+class SymbolicSchurSystem():
+    
+    def __init__(self, T):
+        self.T = T
+        self.N = T.shape[0]
+    
+    def construct(self):
         
-    def multiply_matrix(self, M):
-        if self.current_matrix is None:
-            self.current_matrix = M
-        else:
-            if self.current_matrix.shape[1] != M.shape[0]:
-                print '# of cols != # of rows (%d != %d)' % (self.current_matrix.shape[1], M.shape[0])
-                return
-            
-            new_mat = np.zeros([self.current_matrix.shape[0], M.shape[1]], dtype='string')
-            
-            for i in range(self.current_matrix.shape[0]):
-                for j in range(M.shape[1]):
-                    lrow = self.current_matrix[i, :]
-                    rcol = M[:, j]
-                    
-                    entry_str = []
-                    
-                    for k,lterm in enumerate(lrow):
-                        #split left hand entry into additive parts
-                        aparts = lterm.split('+')
-                        #distribute right hand term with left hand terms
-                        strs = []
-                        rterm = rcol[k]
-                        for a in aparts:
-                            strs.append('%s*%s' % (rterm, a))
-                        entry_str.append('+'.join(strs))
-                    
-                    #join all the terms together for this entry
-                    new_mat[i, j] = '+'.join(entry_str)
-            
-            self.current_matrix = new_mat
+        #construct symbolic unitary matrix U and it's inverse (conjugate transpose)
+        U = np.zeros([self.N, self.N], dtype='object')
+        Uinv = np.zeros([self.N, self.N], dtype='object')
+        for i in range(self.N):
+            for j in range(self.N):
+                sindx = '%d%d' % (i, j)
+                (a_ij, b_ij) = symbols('a_%s b_%s' % (sindx, sindx), complex=False)
+                U[i, j] = a_ij + I*b_ij
+                Uinv[j, i] = a_ij - I*b_ij
+        self.U = np.matrix(U)
+        self.Uinv = np.matrix(Uinv)
+        
+        #compute UU^{-1}
+        self.UUinv = self.U * self.Uinv
+        
+        #compute UTU^{-1}
+        self.UTUinv = self.U * self.T * self.Uinv
+    
+        #construct polynomial strings from the imaginary part of UTU^{-1}
+        eqs = []
+        p = re.compile(r're\(.*\)*')
+        for i in range(self.N):
+            for j in range(self.N):
+                eq_str = str(im(self.UTUinv[i, j].expand()))
+                istr = p.findall(eq_str)[0][3:-1]
+                eqs.append(istr)
+        
+        #construct polynomials from UU^{-1} - I
+        for i in range(self.N):
+            for j in range(self.N):
+                eq = self.UUinv[i, j]
+                if i == j:
+                    eq -= 1.0
+                eq_str = str(eq.expand())
+                eq_str = eq_str.replace('I', 'i')
+                eqs.append(eq_str)
+                
+        self.polynomials = eqs
+        
