dbe2ceb5ec396c3bff307e0221007a3ea1f50b90
mschachter <mike2>
2012-02-26 06:20:50 -0800

    debugging the schur system of equations...

--- src/python/prorn/algebra.py
+++ src/python/prorn/algebra.py
@@ -1,14 +1,17 @@
 import re
+import copy
 
 import numpy as np
 
 from sympy import Symbol, I, symbols, im
+from sympy.parsing.sympy_parser import parse_expr
 
 class SymbolicSchurSystem():
     
     def __init__(self, T):
         self.T = T
         self.N = T.shape[0]
+        self.construct()
     
     def construct(self):
         
@@ -46,8 +49,63 @@ class SymbolicSchurSystem():
                 if i == j:
                     eq -= 1.0
                 eq_str = str(eq.expand())
-                eq_str = eq_str.replace('I', 'i')
                 eqs.append(eq_str)
                 
         self.polynomials = eqs
+    
+    def create_symbolic_T(self):
+        T = np.zeros([self.N, self.N], dtype='object')
+        for i in range(self.N):
+            for j in range(self.N):
+                sindx = '%d%d' % (i, j)
+                (a_ij, b_ij) = symbols('ta_%s tb_%s' % (sindx, sindx), complex=False)
+                T[i, j] = a_ij + I*b_ij
+        return T
+        
+
+    def test(self, U):
+        """ Test the system of equations for a given U """
+        uvals = {}
+        for i in range(self.N):
+            for j in range(self.N):
+                sindx = '%d%d' % (i, j)
+                a = U[i, j].real
+                b = U[i, j].imag
+                uvals['a_%s' % sindx] = '(%0.16f)' % a 
+                uvals['b_%s' % sindx] = '(%0.16f)' % b
+        
+        for eq in self.polynomials:
+            eqnum = copy.copy(eq)
+            for vname,vval in uvals.iteritems():
+                eqnum = eqnum.replace(vname, vval)
+            eqval = parse_expr(eqnum)
+            
+            print '---------------'
+            print 'Expression: %s' % eq
+            #print 'Filled in: %s' % eqnum
+            print 'Value: %s' % str(eqval)
+
+    def to_bertini(self, output_file):
+        f = open(output_file, 'w')
+        f.write('CONFIG\n')
+        f.write('END;\n')
+        f.write('INPUT\n')
+        f.write('variable_group ')
+        vgrps = []
+        for i in range(self.N):
+            for j in range(self.N):
+                sindx = '%d%d' % (i, j)
+                vgrps.append('a_%s' % sindx)
+                vgrps.append('b_%s' % sindx)
+        f.write('%s;\n' % ','.join(vgrps))
+        f.write('function ')
+        nfuncs = 2*self.N**2
+        fstrs = ['f%d' % k for k in range(nfuncs)]
+        f.write('%s;\n' % ','.join(fstrs))
+        
+        for k,eq in self.polynomials:
+            eq.replace('**', '^')
+            f.write('%s=%s;\n' % (fstrs[k], eq))
+        f.write('END;')
+        f.close()
         
